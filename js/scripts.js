aa5a3adf7b435547c561efcec6d512ad1ad770dc
Ethan Bear <ehwbear@icloud.com>
2021-10-19 13:50:07 -0700

    Toggle walrus image works.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").fadeIn(5000);
+  });  
+  $(".clickable").click(function() {
+    $("#walrus-showing").fadeOut(5000);
+  });
+});
+
